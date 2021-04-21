38cd5f0645163996e17a8b2bc1dd903d9240f5ff
Oren93 <54531718+Oren93@users.noreply.github.com>
2021-04-21 12:37:16 +0000

    almost ready

--- src/application/TheControllerOFAllControllers.java
+++ src/application/TheControllerOFAllControllers.java
@@ -7,7 +7,6 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
-import application.Parameters;
 import daytour.*;
 import flights.*;
 import hotel.*;
@@ -37,8 +36,7 @@ public class TheControllerOFAllControllers {
 	}
 	
 	static ObservableList<TourPackage> findDeals (Parameters p) {
-		//ObservableList<ObservableList<Tour>> trips = FXCollections.observableArrayList();
-		ObservableList<Tour> trips = FXCollections.observableArrayList();
+		ObservableList<ObservableList<Tour>> trips = FXCollections.observableArrayList();
 		ObservableList<TourPackage> TP = FXCollections.observableArrayList();
         int days = (int) p.getcheckIn().until(p.getcheckOut(), ChronoUnit.DAYS)-2;
 		initData(p);
@@ -51,17 +49,15 @@ public class TheControllerOFAllControllers {
 				budget -= f.getBasePrice();
 				for (Flight r : returnFlights) {
 					budget -= r.getBasePrice();
-					//trips = makeUniquePlan(p.getcheckIn().plusDays(1),budget,4);
-					trips = searchTours(p.getcheckIn().plusDays(1),budget,4);
-					// the number 4 is for testing, in reality it'd be called:
-					//trips = makeUniquePlan(p.getcheckIn().plusDays(1),budget,days);
-					//for (ObservableList<Tour> lis : trips)
-						//TP.add(new TourPackage(f,r,h,lis));
+					trips = makeUniquePlan(p.getcheckIn().plusDays(1),budget,days);
+					 System.out.println(f.getBasePrice());
+					for (ObservableList<Tour> lis : trips)
+						TP.add(new TourPackage(f,r,h,lis));
 				}
 			}
 		}
 		int k = TP.size();
-		/*for (int i = 0 ; i < k ; i++) {
+		for (int i = 0 ; i < k ; i++) {
 			 System.out.println(i+". i TESTING...........");
 			for (int j = i+1 ;j < k ; ) {
 				 System.out.println(j+". j TESTING...........");
@@ -71,7 +67,7 @@ public class TheControllerOFAllControllers {
 				}
 				else j++;
 			}
-		}*/
+		}
 		return TP;		
 	}
 	private static ObservableList<ObservableList<Tour>> makeUniquePlan(LocalDate d,int budget, int days){
@@ -79,22 +75,21 @@ public class TheControllerOFAllControllers {
 		 ObservableList<ObservableList<Tour>> matrix = FXCollections.<ObservableList<Tour>>observableArrayList();
 		 int currentDay = 0;
 		 int price = 0;
-		 for (int i = 0; i <= lim-days; i++) {
+		 for (int i = 0; i < lim-days; i++) {
 		     ObservableList<Tour> row = FXCollections.<Tour>observableArrayList();
-			 System.out.println(tours.get(0).getDates().toString());
-		     if (tours.get(i).getDates().contains(d.plusDays(currentDay))) {
+		     //if (tours.get(i).getDates().contains(d.plusDays(currentDay))) {
 		    	 row.add(tours.get(i));
 		    	 currentDay++;
 		    	 price += tours.get(i).getPrice();
-		     }
+		    // }
 		     for (int j = i; j < lim; j++) {
-			     if (tours.get(j).getDates().contains(d.plusDays(currentDay))) {
+			 //    if (tours.get(j).getDates().contains(d.plusDays(currentDay))) {
 			    	 row.add(tours.get(j));
 			    	 currentDay++;
 			    	 price += tours.get(j).getPrice();
 			    	 if (currentDay >= days)
 			    		 break;
-			     }
+			 //    }
 		     }
 		     if (price < budget)
 		    	 matrix.add(i, row);
@@ -102,30 +97,5 @@ public class TheControllerOFAllControllers {
 		 }
 		 //System.out.println("Package found: "+matrix.size());
 		 return matrix;
-	}
-	boolean bookPackage(TourPackage p, Passenger pass, Parameters para){
-		boolean disability = false;
-		if (para.getdifficulty() == 10) disability = true;
-		ObservableList<Seat> availableSeatsDepart = getAvailableSeats(p.getFlights().get(0));
-		ObservableList<Seat> availableSeatsReturn = getAvailableSeats(p.getFlights().get(1));
-
-		ObservableList<Seat> SeatsDepart = FXCollections.observableArrayList();
-		ObservableList<Seat> SeatsReturn = FXCollections.observableArrayList();
-
-		for(int i = 0; i<para.getgroupSize(); i++) {
-			SeatsDepart.add(availableSeatsDepart.get(i));
-			SeatsReturn.add(availableSeatsReturn.get(i));
-		}
-
-		flightC.bookFlight(p.getFlights().get(0).getFlightNumber(), SeatsDepart, pass, 0, 0, 0, 0, disability);
-		flightC.bookFlight(p.getFlights().get(1).getFlightNumber(), SeatsReturn, pass, 0, 0, 0, 0, disability);
-
-		hotelC.bookRoom(p.getRoom().getHotelName(), p, pass);
-
-		String customerEmail = "";
-
-		for(int i=0; i<tourDates.size(); i++) { //tourDates needs to be kept somewhere
-			ReservationController.confirmBooking(searchTours(para).get(i), tourDates.get(i), p.getgroupSize(), pass.getLastName(), customerEmail);
-		}
 	}
 }
